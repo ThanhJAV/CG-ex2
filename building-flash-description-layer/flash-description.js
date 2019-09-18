5e84c7feb4a3cb8669bea6e8061d22592b6a1ed6
= <=>
2019-09-18 14:06:45 +0700

    flash

---
+++ building-flash-description-layer/flash-description.js
@@ -0,0 +1,63 @@
+/*let Battery = function () {
+    this.setEnergy = (energy) => {
+        this.energy = energy;
+    };
+    this.getEnergy = () => {
+        return this.energy;
+    };
+    this.decreaseEnergy = () => {
+        if (this.energy > 0) {
+            this.energy--;
+        }
+    };
+    this.decreaseEnergy = () => {
+        if (this.energy > 0) {
+            this.energy--;
+        }
+
+    };
+}
+
+let FlashLamp = () => {
+        this.setBattery = (battery) => {
+            this.battery = battery;
+        };
+        this.getBatteryInfo = () => {
+            return this.battery.getEnergy();
+        };
+        this.light = () => {
+            if(this.status){
+                alert("Lighting");
+            }else{
+                alert("Not linghting");
+            }
+        };
+        this.turnOff = () => {
+            this.status = true;
+        };
+        this.turnOff = () => {
+            this.status = flase;
+        };
+}
+
+let battery = new Battery();
+battery.setEnergy(10);
+
+let flashLamp = newFlashLamp();
+flash.setBattery(battery);
+document.write("Battery info :" + flashLamp.getBatteryInfo() +"<br>");
+flashLamp.light();
+
+document.write("Turn on<br/>");
+flashLamp.turnOn();
+flashLamp.light();
+document.write("Battery info:" + flashLamp.getBatteryInfo() + "<br/>");
+
+document.write("Turn off<br/>");
+flashLamp.turnOff();
+flashLamp.light();
+*/
+
+
+
+
